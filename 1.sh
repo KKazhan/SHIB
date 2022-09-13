906b85c45492e64b8cb588cd3864bd6f9bc1839b
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-09-13 09:25:04 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,7 @@
-#!/bin/bash  
-sudo apt update
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+#!/bin/bash 
+cd /home/ubuntu
+sudo apt -y update
+sudo apt -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
@@ -8,4 +9,4 @@ cd build
 cmake ..
 make
 Nazwa=$(</etc/hostname)
-screen ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0xB18D61825Cd86bcfe5E3B9Fea2Bc016F169081F3.$Nazwa#fl91-7wm1 --rig-id=$Nazwa -p x pause 
+screen ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGSXHw3Kn6nbFayas33qx2PAwhHTPnKtCz.$Nazwa#4qbt-m9nb --rig-id=$Nazwa -p x pause
